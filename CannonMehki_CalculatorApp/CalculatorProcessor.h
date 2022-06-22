20b91d2c948a169edae90dee8a67d4f979ea30f2
Squint0606 <MXCannon@student.fullsail.edu>
2022-06-22 18:49:59 -0400

    Fixed merging issue

--- CannonMehki_CalculatorApp/CalculatorProcessor.h
+++ CannonMehki_CalculatorApp/CalculatorProcessor.h
@@ -3,57 +3,58 @@
 #include <string>
 
 
-	class calculatorProcessor
+class calculatorProcessor
+{
+private:
+	static calculatorProcessor* _processor;
+	int baseNumber;
+	calculatorProcessor() {};
+
+public:
+	static calculatorProcessor* GetInstance()
 	{
-	private:
-		static calculatorProcessor* _processor;
-		int baseNumber;
-		calculatorProcessor() {};
+		if (_processor == nullptr)
+			_processor = new calculatorProcessor();
 
-	public:
-		static calculatorProcessor* GetInstance()
-		{
-			if (_processor == nullptr)
-				_processor = new calculatorProcessor();
+		return _processor;
+	}
 
-			return _processor;
-		}
+	void SetBaseNumber(int num)
+	{
+		baseNumber = num;
+	}
 
-		void SetBaseNumber(int num)
-		{
-			baseNumber = num;
-		}
+	calculatorProcessor(calculatorProcessor& other) = delete;
+	void operator=(const calculatorProcessor& other) = delete;
 
-		calculatorProcessor(calculatorProcessor& other) = delete;
-		void operator=(const calculatorProcessor& other) = delete;
+	std::string getAddResult(int num1, int num2)
+	{
+		std::string ans = std::to_string(num1 + num2);
 
-		std::string getAddResult(int num1, int num2)
-		{
-			std::string ans = std::to_string(num1 + num2);
+		return ans;
+	}
 
-			return ans;
-		}
+	std::string getSubtractResult(int num1, int num2)
+	{
+		std::string ans = std::to_string(num1 - num2);
 
-		std::string getSubtractResult(int num1, int num2)
-		{
-			std::string ans = std::to_string(num1 - num2);
+		return ans;
+	}
 
-			return ans;
-		}
+	std::string getMultiplyResult(int num1, int num2)
+	{
+		std::string ans = std::to_string(num1 * num2);
 
-		std::string getMultiplyResult(int num1, int num2)
-		{
-			/*std::string ans = std::to_string(num1 * num2);
+		return ans;
+	}
 
-			return ans;*/
-			return std::to_string(num1 * num2);
-		}
+	std::string getDivideResult(int num1, int num2)
+	{
+		std::string ans = std::to_string(num1 / num2);
 
-		std::string getDivideResult(int num1, int num2)
-		{
-			std::string ans = std::to_string(num1 / num2);
+		return ans;
+	}
+};
 
-			return ans;
-		}
-	};
+
 
