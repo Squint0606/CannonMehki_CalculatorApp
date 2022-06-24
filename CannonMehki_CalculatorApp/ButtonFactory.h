4fd89ec802ac35e9409dd0b1e3db8f8addd4b245
Squint0606 <MXCannon@student.fullsail.edu>
2022-06-24 11:32:21 -0400

    Save

--- CannonMehki_CalculatorApp/ButtonFactory.h
+++ CannonMehki_CalculatorApp/ButtonFactory.h
@@ -5,27 +5,5 @@ class ButtonFactory
 {
 public:
 	wxButton* createButton(wxWindow* window, int ID, wxString symbol, int x, int y, int length, int width);
-
-	/*wxButton* setButton1();
-	wxButton* setButton2();
-	wxButton* setButton3();
-	wxButton* setButton4();
-	wxButton* setButton5();
-	wxButton* setButton6();
-	wxButton* setButton7();
-	wxButton* setButton8();
-	wxButton* setButton9();
-	wxButton* setButton10();
-	wxButton* setButton11();
-	wxButton* setButton12();
-	wxButton* setButton13();
-	wxButton* setButton14();
-	wxButton* setButton15();
-	wxButton* setButton16();
-	wxButton* setButton17();
-	wxButton* setButton18();
-	wxButton* setButton19();
-	wxButton* setButton20();
-	wxButton* setButton21();*/
 };
 
